69febebc49e05bdb266dd1255d9629106ea3265a
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-10-30 16:22:44 -0400

    atividadeUpdate

--- src/component/ListaProdutos.jsx
+++ src/component/ListaProdutos.jsx
@@ -10,11 +10,11 @@ export default function ListaProdutos({lista}){
             <ul className={styles.blocoCards} key={produto.id}>
               <img src={produto.image} alt={produto.title} width={300} height={300}/>
               <h2>{produto.title} </h2>
-              <li>Preço: ${produto.price}</li>
+              <li className={styles.preco}>Price: ${produto.price}</li>
               <li>Category: {produto.category}</li>
               <li>Rating rate: {produto.rating.rate}</li>
               <li>Rating count: {produto.rating.count}</li>
-              <li className={styles.list}>Description: {produto.description}</li>
+              <li className={styles.list}>{/*Description: {produto.description} */}</li>
             </ul>
           ))}
         </div>
